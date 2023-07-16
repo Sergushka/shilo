beb3eed62584deed6482a74c2347b67b79f58799
Sergushka <39465148+Sergushka@users.noreply.github.com>
2023-07-17 02:16:43 +0300

    Add intersectionObserver to handle infinite scrolling

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,14 +1,48 @@
 <script>
+	import { onMount } from 'svelte';
+
 	/**
 	 * @type {string[]}
 	 */
 	export let urls = [];
+
+	const offset = 3;
+	let index = 0;
+	/**
+	 * @type {HTMLImageElement}
+	 */
+	let imgEl;
+	/**
+	 * @type {IntersectionObserver}
+	 */
+	let observer;
+	$: limitedUrls = urls.slice(offset * 0, offset * (index + 1));
+	onMount(() => {
+		observer = new IntersectionObserver(
+			(entries) => {
+				const first = entries[0];
+				if (first.isIntersecting) {
+					observer.unobserve(first.target);
+					index += 1;
+				}
+			},
+			{ threshold: 0.8 }
+		);
+		observer.observe(imgEl);
+	});
+	$: {
+		if (imgEl) {
+			imgEl.onload = (e) => {
+				observer.observe(e.target);
+			};
+		}
+	}
 </script>
 
 <div class="gallery">
-	{#each urls as url}
+	{#each limitedUrls as url, i}
 		<div class="container">
-			<img class="img" loading="lazy" src={url} on:click alt="SHILO" />
+			<img bind:this={imgEl} class="img" src={url} on:click alt="SHILO" />
 		</div>
 	{/each}
 </div>
